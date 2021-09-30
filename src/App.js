3c47514eb6392116af4512d0f365891ffc9149d7
sifat-fahimul <mdsifatfeni@gmail.com>
2021-10-01 01:52:47 +0600

    update and impliment  navlink in header component

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import Shop from './components/Shop/Shop';
 function App() {
   return (
     <div>
-      <Header></Header>
+
       <Router>
+        <Header></Header>
         <Switch>
           <Route exact path='/'>
             <Shop></Shop>
